9237b9d067adf1262ec092389be0840791d98bd0
brown <450427963@qq.com>
2022-04-30 16:30:36 +0800

    update cache

--- LearnOperation/LearnOperation/MyModel.swift
+++ LearnOperation/LearnOperation/MyModel.swift
@@ -6,8 +6,56 @@
 //
 
 import Foundation
+import UIKit
+import CommonCrypto
+
 
 struct MyModel {
     let imageUrl: String
     let title: String
+}
+
+extension String {
+    func getDownloadURL() -> URL {
+     
+        let fileManager = FileManager.default
+        let cacheURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let cacheDir = cacheURL.appendingPathComponent("LGDiskPath", isDirectory: true)
+        let isExist = fileManager.fileExists(atPath: cacheDir.path)
+        if !isExist {
+            do {
+                try fileManager.createDirectory(atPath: cacheDir.path, withIntermediateDirectories: true, attributes:nil)
+            } catch {
+                print("cannot create folders \(error)")
+            }
+        }
+        
+        return cacheDir.appendingPathComponent(md5, isDirectory: false)
+        
+    
+    }
+
+    /// 原生md5
+    public var md5: String {
+        guard let data = data(using: .utf8) else {
+            return self
+        }
+        var digest = [UInt8](repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
+
+        #if swift(>=5.0)
+
+        _ = data.withUnsafeBytes { (bytes: UnsafeRawBufferPointer) in
+            CC_MD5(bytes.baseAddress, CC_LONG(data.count), &digest)
+        }
+
+        #else
+
+        _ = data.withUnsafeBytes { bytes in
+            CC_MD5(bytes, CC_LONG(data.count), &digest)
+        }
+
+        #endif
+
+        return digest.map { String(format: "%02x", $0) }.joined()
+    }
 }
--- LearnOperation/LearnOperation/ViewController.swift
+++ LearnOperation/LearnOperation/ViewController.swift
@@ -35,22 +35,21 @@ class ViewController: UIViewController {
     private var viewModel: MyViewModel?
     
     private var dataArray: [MyModel] = []
+    private var cacheImageMap: [String: UIImage] = [:]
+    
+    private let operationQueue = OperationQueue()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(collectionView)
+        view.addSubview(self.collectionView)
         
-        viewModel = MyViewModel(success: {[weak self] data in
+        self.viewModel = MyViewModel(success: { [weak self] data in
             self?.dataArray.append(contentsOf: data)
             self?.collectionView.reloadData()
-        }, failure: { data in
+        }, failure: { _ in
             
         })
-        
-        
     }
-
-
 }
 
 extension ViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
@@ -61,12 +60,57 @@ extension ViewController: UICollectionViewDataSource, UICollectionViewDelegateFl
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyCollectionViewCell.identifier, for: indexPath) as! MyCollectionViewCell
         
-        let model = self.dataArray[indexPath.row]
+        let model: MyModel = self.dataArray[indexPath.row]
         let imageURL = URL(string: model.imageUrl)!
         cell.titleLabel.text = model.title
         
-        let imageData = try! Data(contentsOf: imageURL)
-        cell.imageView.image = UIImage(data: imageData)
+        // 内存缓存 -- 内存会清理
+        if let image = cacheImageMap[model.imageUrl] {
+            print("使用内存缓存: \(model.title)")
+            cell.imageView.image = image
+            return cell
+        }
+        
+        // 沙盒缓存
+        let cacheImageURL = model.imageUrl.getDownloadURL()
+        let diskImage = UIImage(contentsOfFile: cacheImageURL.path)
+        if let image = diskImage {
+            print("使用磁盘缓存: \(model.title)")
+            cell.imageView.image = image
+            return cell
+        }
+        
+        print("开始后台下载: \(model.title)")
+        // 下载 - 事务
+        // 开辟子线程 - 下载
+        let bo = BlockOperation {
+            print("去下载: \(model.title)")
+            // 下载
+            let imageData = try! Data(contentsOf: imageURL)
+            // 写入diskCache缓存
+            try! imageData.write(to: cacheImageURL)
+            
+            let image = UIImage(data: imageData)
+            
+            // 更新UI
+            OperationQueue.main.addOperation {
+                // 缓存内存
+                self.cacheImageMap[model.imageUrl] = image
+                // 缓存磁盘
+                
+                
+                // 更新UI
+                cell.imageView.image = image
+            }
+        }
+            
+        self.operationQueue.addOperation(bo)
+        
         return cell
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        self.cacheImageMap.removeAll()
     }
 }
